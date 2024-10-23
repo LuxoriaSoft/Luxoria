605e53b994f21d89ac686b317791454904d3cb35
CPU64x <q.erdinger@orange.fr>
2024-10-23 18:10:35 +0100

    fix: Update commitlint.config.js to enforce subject case rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,13 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-max-line-length': [0, 'always', 150], // Set limit to 150 (or any other value you prefer)
+    'subject-case': [
+      2, // Set to 2 for error (1 for warning)
+      'always',
+      [
+        'lower-case', // Ensure the subject is in lowercase before the colon
+        'sentence-case', // Allow first letter after the colon to be uppercase
+      ],
+    ],
   },
 };
-  
