c9e2d1f840d57aa4fa6abd111e13b460eea23816
QuentinCraft <quentin.erdinger@epitech.eu>
2024-12-28 17:47:06 +0100

    fix: Add multi-thread process on compute function
    Thanks to Multi-tasking and multi-threading, the compute function now enables these features to reduce time consuming and improve the performance of the function

--- Modules/LuxFilter/LuxFilter/Services/PipelineService.cs
+++ Modules/LuxFilter/LuxFilter/Services/PipelineService.cs
@@ -3,6 +3,7 @@ using LuxFilter.Interfaces;
 using Luxoria.SDK.Interfaces;
 using SkiaSharp;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -41,39 +42,53 @@ namespace LuxFilter.Services
                 throw new InvalidOperationException("Pipeline has no algorithms to execute.");
             }
 
+            DateTime start = DateTime.Now;
             _logger.Log("Executing pipeline...");
-            var allBitmapScores = new List<double>(); // To store the final score for each bitmap
 
-            foreach (var bitmapWithSize in bitmapsWithSizes)
+            var indexedBitmaps = bitmapsWithSizes
+                .Select((bitmap, index) => (Index: index, BitmapWithSize: bitmap))
+                .ToList();
+
+            var results = new ConcurrentDictionary<int, double>();
+
+            await Task.Run(() =>
             {
-                double fscore = 0; // Final score for this bitmap
-                int totalAlgo = _workflow.Count;
-
-                // Execute each algorithm on the current bitmap
-                foreach (var step in _workflow)
+                Parallel.ForEach(indexedBitmaps, indexedBitmap =>
                 {
-                    IFilterAlgorithm algorithm = step.Item1;
-                    double weight = step.Item2;
+                    var (index, bitmapWithSize) = indexedBitmap;
 
-                    _logger.Log($"Executing algorithm: [{algorithm.Name}] (w={weight}) on bitmap of size {bitmapWithSize.Width}x{bitmapWithSize.Height}...");
-                    try
+                    double fscore = 0;
+
+                    foreach (var step in _workflow)
                     {
-                        var score = algorithm.Compute(bitmapWithSize.Bitmap, bitmapWithSize.Height, bitmapWithSize.Width);
-                        _logger.Log($"Score for algorithm [{algorithm.Name}] on bitmap: {score}");
-                        fscore += score * weight;
-                    }
-                    catch (Exception ex)
-                    {
-                        _logger.Log($"Error executing [{algorithm.Name}] on bitmap: {ex.Message}");
-                    }
-                }
+                        IFilterAlgorithm algorithm = step.Item1;
+                        double weight = step.Item2;
+                        DateTime startingTime = DateTime.Now;
 
-                _logger.Log($"Final score for bitmap: {fscore}");
-                allBitmapScores.Add(fscore);
-            }
+                        _logger.Log($"Executing algorithm: [{algorithm.Name}] (w={weight}) (thrd={Thread.CurrentThread.ManagedThreadId})...");
+                        try
+                        {
+                            var score = algorithm.Compute(bitmapWithSize.Bitmap, bitmapWithSize.Height, bitmapWithSize.Width);
+                            DateTime endingTime = DateTime.Now;
+                            _logger.Log($"Score for algorithm [{algorithm.Name}] on bitmap: {score}, time consumed : ({endingTime - startingTime})s");
+                            fscore += score * weight;
+                        }
+                        catch (Exception ex)
+                        {
+                            _logger.Log($"Error executing [{algorithm.Name}] on bitmap: {ex.Message}");
+                        }
+                    }
 
-            _logger.Log("Pipeline execution completed.");
-            return allBitmapScores;
+                    _logger.Log($"Final score for bitmap at index {index}: {fscore}");
+                    results.TryAdd(index, fscore);
+                });
+            });
+
+            DateTime end = DateTime.Now;
+            _logger.Log($"Pipeline execution completed (time consumed = {end - start})s.");
+
+            // Return results ordered by index
+            return results.OrderBy(kvp => kvp.Key).Select(kvp => kvp.Value).ToList();
         }
 
     }
