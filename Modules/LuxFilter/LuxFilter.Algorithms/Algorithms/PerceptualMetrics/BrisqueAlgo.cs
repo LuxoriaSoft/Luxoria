2f541ef68093d38afa9bbd18198ed48bcc025f42
QuentinCraft <quentin.erdinger@epitech.eu>
2025-02-03 11:30:51 +0000

    refactor: Removed Unused nugets on LuxFilter.Algorithms/Tests

--- Modules/LuxFilter/LuxFilter.Algorithms/Algorithms/PerceptualMetrics/BrisqueAlgo.cs
+++ Modules/LuxFilter/LuxFilter.Algorithms/Algorithms/PerceptualMetrics/BrisqueAlgo.cs
@@ -1,108 +1,105 @@
 ﻿using LuxFilter.Algorithms.Interfaces;
 using Luxoria.Algorithm.BrisqueScore;
 using SkiaSharp;
-using System;
-using System.IO;
 using System.Reflection;
 
-namespace LuxFilter.Algorithms.PerceptualMetrics
+namespace LuxFilter.Algorithms.PerceptualMetrics;
+
+public class BrisqueAlgo : IFilterAlgorithm, IDisposable
 {
-    public class BrisqueAlgo : IFilterAlgorithm, IDisposable
+    /// <summary>
+    /// Get the algorithm name
+    /// </summary>
+    public string Name => "Brisque";
+
+    /// <summary>
+    /// Get the algorithm description
+    /// </summary>
+    public string Description => "Brisque algorithm";
+
+    private readonly BrisqueInterop _brisque;
+    private readonly string _modelPath;
+    private readonly string _rangePath;
+
+    /// <summary>
+    /// Constructor - Initializes BrisqueInterop with embedded YAML files.
+    /// </summary>
+    public BrisqueAlgo()
     {
-        /// <summary>
-        /// Get the algorithm name
-        /// </summary>
-        public string Name => "Brisque";
-
-        /// <summary>
-        /// Get the algorithm description
-        /// </summary>
-        public string Description => "Brisque algorithm";
-
-        private readonly BrisqueInterop _brisque;
-        private readonly string _modelPath;
-        private readonly string _rangePath;
-
-        /// <summary>
-        /// Constructor - Initializes BrisqueInterop with embedded YAML files.
-        /// </summary>
-        public BrisqueAlgo()
+        try
         {
-            try
-            {
-                _modelPath = ExtractEmbeddedResource("LuxFilter.Algorithms.Algorithms.PerceptualMetrics.brisque_model_live.yml");
-                _rangePath = ExtractEmbeddedResource("LuxFilter.Algorithms.Algorithms.PerceptualMetrics.brisque_range_live.yml");
+            _modelPath = ExtractEmbeddedResource("LuxFilter.Algorithms.Algorithms.PerceptualMetrics.brisque_model_live.yml");
+            _rangePath = ExtractEmbeddedResource("LuxFilter.Algorithms.Algorithms.PerceptualMetrics.brisque_range_live.yml");
 
-                _brisque = new BrisqueInterop(_modelPath, _rangePath);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Error initializing Brisque: {e.Message}");
-                throw;
-            }
+            _brisque = new BrisqueInterop(_modelPath, _rangePath);
         }
-
-        /// <summary>
-        /// Compute the Brisque score of the image.
-        /// </summary>
-        /// <param name="bitmap"></param>
-        /// <param name="height"></param>
-        /// <param name="width"></param>
-        /// <returns>Returns the computed score of the algorithm</returns>
-        public double Compute(SKBitmap bitmap, int height, int width)
+        catch (Exception e)
         {
-            try
-            {
-                string imagePath = SaveBitmapToTempFile(bitmap);
-                return _brisque.ComputeScore(imagePath);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Error computing Brisque score: {e.Message}");
-                return 0;
-            }
+            Console.WriteLine($"Error initializing Brisque: {e.Message}");
+            throw;
         }
+    }
 
-        /// <summary>
-        /// Saves an SKBitmap to a temporary file as a PNG.
-        /// </summary>
-        /// <param name="bitmap">The SKBitmap to save</param>
-        /// <returns>The file path of the saved image</returns>
-        private string SaveBitmapToTempFile(SKBitmap bitmap)
+    /// <summary>
+    /// Compute the Brisque score of the image.
+    /// </summary>
+    /// <param name="bitmap"></param>
+    /// <param name="height"></param>
+    /// <param name="width"></param>
+    /// <returns>Returns the computed score of the algorithm</returns>
+    public double Compute(SKBitmap bitmap, int height, int width)
+    {
+        try
         {
-            string tempFile = Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid()}.png");
-
-            using SKFileWStream fileStream = new SKFileWStream(tempFile);
-            bitmap.Encode(fileStream, SKEncodedImageFormat.Png, 100);
-
-            return tempFile;
+            string imagePath = SaveBitmapToTempFile(bitmap);
+            return _brisque.ComputeScore(imagePath);
         }
-
-        /// <summary>
-        /// Extracts an embedded resource and writes it to a temporary file.
-        /// </summary>
-        /// <param name="resourceName">The resource name</param>
-        /// <returns>Path to the extracted file</returns>
-        private string ExtractEmbeddedResource(string resourceName)
+        catch (Exception e)
         {
-            Assembly assembly = Assembly.GetExecutingAssembly();
-            using Stream? stream = assembly.GetManifestResourceStream(resourceName);
-            if (stream == null)
-                throw new FileNotFoundException($"Embedded resource {resourceName} not found");
-
-            string tempFile = Path.Combine(Path.GetTempPath(), Path.GetFileName(resourceName));
-            using FileStream fileStream = new FileStream(tempFile, FileMode.Create, FileAccess.Write);
-            stream.CopyTo(fileStream);
-
-            return tempFile;
+            Console.WriteLine($"Error computing Brisque score: {e.Message}");
+            return 0;
         }
+    }
 
-        /// <summary>
-        /// Properly disposes of the BrisqueInterop instance.
-        /// </summary>
-        public void Dispose()
-        {
-            _brisque?.Dispose();
-        }
+    /// <summary>
+    /// Saves an SKBitmap to a temporary file as a PNG.
+    /// </summary>
+    /// <param name="bitmap">The SKBitmap to save</param>
+    /// <returns>The file path of the saved image</returns>
+    private string SaveBitmapToTempFile(SKBitmap bitmap)
+    {
+        string tempFile = Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid()}.png");
+
+        using SKFileWStream fileStream = new SKFileWStream(tempFile);
+        bitmap.Encode(fileStream, SKEncodedImageFormat.Png, 100);
+
+        return tempFile;
+    }
+
+    /// <summary>
+    /// Extracts an embedded resource and writes it to a temporary file.
+    /// </summary>
+    /// <param name="resourceName">The resource name</param>
+    /// <returns>Path to the extracted file</returns>
+    private string ExtractEmbeddedResource(string resourceName)
+    {
+        Assembly assembly = Assembly.GetExecutingAssembly();
+        using Stream? stream = assembly.GetManifestResourceStream(resourceName);
+        if (stream == null)
+            throw new FileNotFoundException($"Embedded resource {resourceName} not found");
+
+        string tempFile = Path.Combine(Path.GetTempPath(), Path.GetFileName(resourceName));
+        using FileStream fileStream = new FileStream(tempFile, FileMode.Create, FileAccess.Write);
+        stream.CopyTo(fileStream);
+
+        return tempFile;
+    }
+
+    /// <summary>
+    /// Properly disposes of the BrisqueInterop instance.
+    /// </summary>
+    public void Dispose()
+    {
+        _brisque?.Dispose();
     }
 }
