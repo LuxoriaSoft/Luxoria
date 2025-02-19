fbd5489d40bc8a3f4bde2fada81abc7c82d15bdc
QuentinCraft <quentin.erdinger@epitech.eu>
2025-02-19 12:59:36 +0000

    docs: Add documentations inside each event and create an Event README

--- Luxoria.App/Luxoria.Modules/Models/Events/RequestWindowHandleEvent.cs
+++ Luxoria.App/Luxoria.Modules/Models/Events/RequestWindowHandleEvent.cs
@@ -1,18 +1,24 @@
 ﻿using Luxoria.Modules.Interfaces;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Luxoria.Modules.Models.Events
 {
+    /// <summary>
+    /// Event used to request a window handle.
+    /// The provided callback will be invoked with the retrieved window handle.
+    /// </summary>
     [ExcludeFromCodeCoverage]
     public class RequestWindowHandleEvent : IEvent
     {
+        /// <summary>
+        /// Gets the callback action that will be invoked when the window handle is received.
+        /// </summary>
         public Action<nint>? OnHandleReceived { get; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RequestWindowHandleEvent"/> class.
+        /// </summary>
+        /// <param name="onHandleReceived">Callback that receives the window handle as a parameter.</param>
         public RequestWindowHandleEvent(Action<nint> onHandleReceived)
         {
             OnHandleReceived = onHandleReceived;
