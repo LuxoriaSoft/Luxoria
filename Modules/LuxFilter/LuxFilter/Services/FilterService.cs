56fc939d04c0900df1ebed423fdbfa18225adf55
QuentinCraft <quentin.erdinger@epitech.eu>
2025-02-20 14:30:04 +0000

    refactor: FilterService class make it static

--- Modules/LuxFilter/LuxFilter/Services/FilterService.cs
+++ Modules/LuxFilter/LuxFilter/Services/FilterService.cs
@@ -7,18 +7,14 @@ namespace LuxFilter.Services;
 public class FilterService
 {
     /// <summary>
-    /// Catalog definition
-    /// [Algorithm Display Name, Factory Function to Create an Instance]
+    /// Static catalog definition
     /// </summary>
-    public ImmutableDictionary<string, IFilterAlgorithm> Catalog { get; }
-
-    public FilterService()
-    {
-        Catalog = ImmutableDictionary.CreateRange(new Dictionary<string, IFilterAlgorithm>
+    public static ImmutableDictionary<string, IFilterAlgorithm> Catalog { get; } = ImmutableDictionary.CreateRange(
+        new Dictionary<string, IFilterAlgorithm>
         {
             { "Resolution", new Algorithms.ImageQuality.ResolutionAlgo() },
             { "Sharpness", new Algorithms.ImageQuality.SharpnessAlgo() },
             { "Brisque", new Algorithms.PerceptualMetrics.BrisqueAlgo() }
-        });
-    }
+        }
+    );
 }
