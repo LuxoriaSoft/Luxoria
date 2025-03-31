af5b7f0234c0b55c323c9dceebbbe713997d4865
Noah <noah.gosciniak@epitech.eu>
2025-03-31 14:26:45 +0900

    Fix: Export Pop Up with progress bar and previsualization

---
+++ Modules/LuxExport/LuxExport/Extensions/DispatcherQueueExtension.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI.Dispatching;
+
+public static class DispatcherQueueExtensions
+{
+    public static Task EnqueueAsync(this DispatcherQueue dispatcherQueue, Action action)
+    {
+        var tcs = new TaskCompletionSource<bool>();
+
+        // On enfile l’action sur le thread UI
+        dispatcherQueue.TryEnqueue(() =>
+        {
+            try
+            {
+                action();
+                tcs.SetResult(true);
+            }
+            catch (Exception ex)
+            {
+                tcs.SetException(ex);
+            }
+        });
+
+        return tcs.Task;
+    }
+
+    public static Task<T> EnqueueAsync<T>(this DispatcherQueue dispatcherQueue, Func<T> func)
+    {
+        var tcs = new TaskCompletionSource<T>();
+
+        dispatcherQueue.TryEnqueue(() =>
+        {
+            try
+            {
+                var result = func();
+                tcs.SetResult(result);
+            }
+            catch (Exception ex)
+            {
+                tcs.SetException(ex);
+            }
+        });
+
+        return tcs.Task;
+    }
+}
