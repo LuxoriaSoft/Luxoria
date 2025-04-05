31216ba92e8f79126900bbdd487a8a33e50dcd34
QuentinCraft <91551241+QuentinCraft@users.noreply.github.com>
2025-04-05 14:59:57 +0100

    feat: Add luxportal service to Docker Compose; update routing and error handling in Login component

--- LuxStudio/portal/src/router/index.ts
+++ LuxStudio/portal/src/router/index.ts
@@ -10,6 +10,10 @@ import LinkAccount from '../views/SSO_Authorize.vue';
 const routes = [
   {
     path: '/',
+    redirect: '/dashboard',
+  },
+  {
+    path: '/login',
     name: 'Login',
     component: Login,
   },
@@ -65,7 +69,7 @@ router.beforeEach(async (to, _, next) => {
   if (to.meta.requiresAuth) {
     if (!token || isTokenExpired(token)) {
       console.log("Token expired, redirecting to login...");
-      return next({ path: "/", query: { redirect: to.fullPath } });
+      return next({ path: "/login", query: { redirect: to.fullPath } });
     }
   }
 
