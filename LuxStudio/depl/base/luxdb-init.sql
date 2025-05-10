cb15762f8fe7cebe4c91bd601d721f9f107af75b
QuentinCraft <91551241+QuentinCraft@users.noreply.github.com>
2025-05-10 16:56:58 +0100

    refactor: Comment out database creation in SQL initialization scripts

--- LuxStudio/depl/base/luxdb-init.sql
+++ LuxStudio/depl/base/luxdb-init.sql
@@ -1,8 +1,11 @@
-CREATE DATABASE luxoria;
+-- Already created by POSTGRES_DB env var, no need to recreate
+-- CREATE DATABASE luxoria;
+
 \c luxoria;
 
 CREATE USER dbowner WITH PASSWORD 'strongpassword';
 ALTER DATABASE luxoria OWNER TO dbowner;
+
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO dbowner;
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO dbowner;
 GRANT USAGE, CREATE ON SCHEMA public TO dbowner;
