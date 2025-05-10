9f8e645dddaa568a8f36209d7713f251a67fd25c
QuentinCraft <91551241+QuentinCraft@users.noreply.github.com>
2025-05-10 16:27:50 +0100

    refactor: Add LuxDB initialization scripts and update deployment configuration

---
+++ LuxStudio/depl/base/luxdb-init.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE luxoria;
+\c luxoria;
+
+CREATE USER dbowner WITH PASSWORD 'strongpassword';
+ALTER DATABASE luxoria OWNER TO dbowner;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO dbowner;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO dbowner;
+GRANT USAGE, CREATE ON SCHEMA public TO dbowner;
