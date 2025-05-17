42db9f319ee0a2e415bc4ba624fd1bf0ca3738e8
Lucas <lucas.hoch@epitech.eu>
2025-05-17 14:32:28 -0200

    fix: Update API_URL in config files for consistency and environment variable support

--- LuxStudio/portal/docker-entrypoint.sh
+++ LuxStudio/portal/docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Replace placeholder with environment variable
-sed -i "s|__API_URL__|${API_URL:-http://localhost:5269}|g" /usr/share/nginx/html/config.js
+sed -i "s|http://localhost:5269|${API_URL:-http://localhost:5269}|g" /usr/share/nginx/html/config.js
 exec "$@"
