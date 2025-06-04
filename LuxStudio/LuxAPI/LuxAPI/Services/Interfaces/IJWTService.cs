a807a8256ceec479123d46d2852bfa8a3518d794
QuentinCraft <91551241+QuentinCraft@users.noreply.github.com>
2025-06-04 15:17:39 +0100

    fix: Bring docs for IJwtService

--- LuxStudio/LuxAPI/LuxAPI/Services/Interfaces/IJWTService.cs
+++ LuxStudio/LuxAPI/LuxAPI/Services/Interfaces/IJWTService.cs
@@ -1,6 +1,17 @@
 namespace LuxAPI.Services.Interfaces;
 
+/// <summary>
+/// Interface for JWT service
+/// </summary>
 public interface IJwtService
 {
+    /// <summary>
+    /// Generates a JWT token for an authenticated user
+    /// </summary>
+    /// <param name="userId">UserID as Guid</param>
+    /// <param name="username">UserName as String</param>
+    /// <param name="email">Email as String</param>
+    /// <param name="expiryHours">ExpiryHours as Int (default = 48h)</param>
+    /// <returns>Returns the token as String</returns>
     string GenerateJwtToken(Guid userId, string username, string email, int expiryHours = 48);
 }
