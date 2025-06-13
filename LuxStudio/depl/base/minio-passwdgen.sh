c091a3ba6251c04f50676de7dfb60e835a719bde
QuentinCraft <91551241+QuentinCraft@users.noreply.github.com>
2025-06-13 12:56:43 +0100

    fix: Specify namespace for luxoria/minio secret

--- LuxStudio/depl/base/minio-passwdgen.sh
+++ LuxStudio/depl/base/minio-passwdgen.sh
@@ -4,3 +4,4 @@ export MINIO_SECRET_KEY=$(openssl rand -hex 16)  # 32-char hex string
 kubectl create secret generic minio-secret \
   --from-literal=MINIO_ROOT_USER=$MINIO_ACCESS_KEY \
   --from-literal=MINIO_ROOT_PASSWORD=$MINIO_SECRET_KEY
+  -n luxstudio-ENVNAME
