314b12db051a7f77c5c61c013a91aabdaf05c0fd
QuentinCraft <91551241+QuentinCraft@users.noreply.github.com>
2025-06-27 21:50:48 +0200

    fix: Comment out dev front urls

--- LuxStudio/LuxAPI/LuxAPI/Program.cs
+++ LuxStudio/LuxAPI/LuxAPI/Program.cs
@@ -115,8 +115,8 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend", policy =>
     {
         policy.WithOrigins(
-            "http://localhost:3000",
-            "http://localhost:5173",
+            // "http://localhost:3000",
+            // "http://localhost:5173",
             FRONT_URI
         )
         .AllowAnyHeader()
